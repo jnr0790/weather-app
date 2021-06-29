19e78e14bd8ddd0e38e48e371cf53972e86feab4
jnr0790 <jromero0790@gmaill.com>
2021-06-28 21:28:14 -0400

    Adds weather display set-up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Input from './components/Input'
+import Weather from './components/Weather'
 
 function App() {
   return (
     <div className="app">
       <Input />
+      <Weather />
     </div>
   );
 }
